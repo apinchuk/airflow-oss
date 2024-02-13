c75fcc40ec031ee26c41708a159b4ac1bf0e83c2
apinchuk1 <alisa.pinchuk@intel.com>
2024-02-13 14:29:56 -0700

    dags folder added

---
+++ dags/dag_sample.py
@@ -0,0 +1,30 @@
+from airflow import DAG 
+from airflow.operators.bash_operator import BashOperator 
+from airflow.utils.dates import days_ago 
+from airflow.operators.python_operator import PythonOperator 
+
+default_args = { 
+    'owner': 'alisa', 
+    'start_date': days_ago(1) 
+    } 
+
+ 
+# Defining the DAG using Context Manager 
+with DAG( 
+        'bash_write', 
+        default_args=default_args, 
+        schedule_interval=None, 
+        ) as dag: 
+
+
+t1 = BashOperator( 
+                task_id = 'create_task', 
+                bash_command = 'python pi.py'
+        ) 
+
+#start = DummyOperator(task_id='start', dag=dag) 
+end = DummyOperator(task_id='end', dag=dag) 
+
+#step 5 - Define Dependency 
+
+#start >> end 
